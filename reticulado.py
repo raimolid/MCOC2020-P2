bfa0f8a9758df284205f4d1579bf1973bd6bab4c
Jose Abell <ja.abell@gmail.com>
2020-09-25 09:20:46 -0300

    Importacion inicial del código

---
+++ reticulado.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class Reticulado(object):
+	"""Define un reticulado"""
+
+	def __init__(self):
+		super(Reticulado, self).__init__()
+		
+		self.xyz = np.zeros((0,3), dtype=np.double)
+		self.Nnodos = 0
+		self.barras = []
+		self.cargas = {}
+		self.restricciones = {}
+
+	def agregar_nodo(self, x, y, z=0):
+		"""Implementar"""
+		return
+		
+	def agregar_barra(self, barra):
+		"""Implementar"""
+		return
+
+	def obtener_coordenada_nodal(self, n): 
+		"""Implementar"""
+		return 
+
+	def calcular_peso_total(self):
+		"""Implementar"""
+		return 
+
+	def obtener_nodos(self):
+		"""Implementar"""
+		return 
+
+	def obtener_barras(self):
+		"""Implementar"""
+		return 
+
+	def agregar_restriccion(self, nodo, gdl, valor=0.0):
+		"""Implementar"""
+		return
+
+	def agregar_fuerza(self, nodo, gdl, valor):
+		"""Implementar"""
+		return
+
+	def ensamblar_sistema(self):
+		"""Implementar"""
+		return
+
+	def resolver_sistema(self):
+		"""Implementar"""
+		return
+
+	def recuperar_fuerzas(self):
+		"""Implementar"""
+		return
+
