7e80db3c64c01460f71994eeba5d963bb8bfd793
Jose Abell <ja.abell@gmail.com>
2020-09-29 15:51:40 -0300

    * Agregando solucion entrega 1, contenido entrega 2.

--- barra.py
+++ barra.py
@@ -5,46 +5,79 @@ g = 9.81 #kg*m/s^2
 
 class Barra(object):
 
-	"""Constructor para una barra"""
-	def __init__(self, ni, nj, R, t, E, ρ, σy):
-		super(Barra, self).__init__()
-		self.ni = ni
-		self.nj = nj
-		self.R = R
-		self.t = t
-		self.E = E
-		self.ρ = ρ
-		self.σy = σy
+    """Constructor para una barra"""
+    def __init__(self, ni, nj, R, t, E, ρ, σy):
+        super(Barra, self).__init__()
+        self.ni = ni
+        self.nj = nj
+        self.R = R
+        self.t = t
+        self.E = E
+        self.ρ = ρ
+        self.σy = σy
 
-	def obtener_conectividad(self):
-		"""Implementar"""
-		return 
+    def obtener_conectividad(self):
+        return [self.ni, self.nj]
 
-	def calcular_area(self):
-		"""Implementar"""
-		return 
+    def calcular_area(self):
+        A = np.pi*(self.R**2) - np.pi*((self.R-self.t)**2)
+        return A
 
-	def calcular_largo(self, reticulado):
-		"""Devuelve el largo de la barra. 
-		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
-		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
-		"""
-		"""Implementar"""
-		return 
+    def calcular_largo(self, reticulado):
+        """Devuelve el largo de la barra. 
+        ret: instancia de objeto tipo reticulado
+        """
+        xi = reticulado.obtener_coordenada_nodal(self.ni)
+        xj = reticulado.obtener_coordenada_nodal(self.nj)
+        dij = xi-xj
+        return np.sqrt(np.dot(dij,dij))
 
-	def calcular_peso(self, reticulado):
-		"""Devuelve el largo de la barra. 
-		xi : Arreglo numpy de dimenson (3,) con coordenadas del nodo i
-		xj : Arreglo numpy de dimenson (3,) con coordenadas del nodo j
-		"""
-		"""Implementar"""
-		return 
-
-
+    def calcular_peso(self, reticulado):
+        """Devuelve el largo de la barra. 
+        ret: instancia de objeto tipo reticulado
+        """
+        L = self.calcular_largo(reticulado)
+        A = self.calcular_area()
+        return self.ρ * A * L * g
 
 
 
 
+
+
+
+
+
+
+
+    def obtener_rigidez(self, ret):
+        """Devuelve la rigidez ke del elemento. 
+        ret: instancia de objeto tipo reticulado
+        """
+        
+        #implementar
+
+        return ke
+
+    def obtener_vector_de_cargas(self, ret):
+        """Devuelve el vector de cargas nodales fe del elemento. 
+        ret: instancia de objeto tipo reticulado
+        """
+
+        #Implementar
+
+        return fe
+
+
+    def obtener_fuerza(self, ret):
+        """Devuelve la fuerza se que debe resistir la barra 
+        ret: instancia de objeto tipo reticulado
+        """
+
+        #Implementar
+
+
+        return se
 
 
 
